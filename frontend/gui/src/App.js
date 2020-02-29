a3b6f396491aae8661054d8f53fde1288109d00a
Valeria Trotta <valeria.trotta@correo.unimet.edu.ve>
2020-02-29 09:56:53 -0400

    Update App.js

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -6,16 +6,47 @@ import BaseRouter from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import CustomLayout from "./containers/Layout";
 import ProductList from "./containers/ProductListView";
+import ProductScrum from "./components/ProductScrum";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <div className="container">
+          <div id="task-container">
+            <div id="form-wrapper">
+              <form id="form">
+                <div className="flex-wrapper">
+                  <div style={{ flex: 6 }}>
+                    <input
+                      className="form-control"
+                      id="title"
+                      type="text"
+                      name="title"
+                      placeholder="Add task"
+                    ></input>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <input
+                      className="btn btn-warning"
+                      id="sumbit"
+                      type="submit"
+                      name="Add"
+                    ></input>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div id="list-wrapper"></div>
+          </div>
+        </div>
+
+        {/* <Router>
           <CustomLayout>
             <BaseRouter />
           </CustomLayout>
-        </Router>
+        </Router> */}
       </div>
     );
   }
